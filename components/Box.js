9bafb2796ca5a92cc003b8dc7e593cee7c573e8b
Egan Bisma <brickkace@gmail.com>
2020-10-10 00:46:32 -0700

    started calendar box component

---
+++ components/Box.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function Box() {
+    return (
+        <div className="box">
+        </div>
+    );
+}
+
+export default Box;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import Layout from "../components/layout";
+import Box from "../components/Box";
 
 function IndexPage() {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center">
-        <img
-          src="team-of-critters.svg"
-          className="w-full max-w-xl"
-          alt="Four one-eyed aliens playing"
-        />
-
         <h2 className="p-3 my-8 text-lg font-bold bg-yellow-400 md:text-2xl">
-          Hi! Welcome to your first Next.js site.
+          Dune release countdown
         </h2>
+        <div className="grid grid-cols-12 gap-1">
+          {
+            Array(20).fill(0).map((_, index) => {
+              return <Box key={index}></Box>
+            })
+          }
+        </div>
       </div>
     </Layout>
   );
